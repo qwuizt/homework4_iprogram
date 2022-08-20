a7ddce5e9fecf27fe0c033a305a29e03e2f2a208
QWuizt <boringfine@gmail.com>
2022-08-21 01:19:07 +0300

    задание 25

---
+++ task25/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число 'a'");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 'b'");
+int b = Convert.ToInt32(Console.ReadLine());
+int degree = a;
+for ( int number = 2; number <= b; number++)
+{
+    degree*= a;
+}
+Console.WriteLine($"a^b -> {degree}");   
+
+
