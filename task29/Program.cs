ba5f6c4a058928f69d0b39618bc7dca5ed8ca41b
«qwoizt» <boringfine@gmail.com>
2022-08-21 20:45:59 +0300

    задание 29

---
+++ task29/Program.cs
@@ -0,0 +1,9 @@
+﻿int[] myArray = new int[8];
+Random rand = new Random();
+for (int i = 0; i < myArray.Length -1; i++)
+    {
+        myArray[i] = rand.Next(100);
+        Console.Write($"{myArray[i]}, ");
+    }
+myArray[7] = rand.Next(100);
+Console.WriteLine($"{myArray[7]}.");
