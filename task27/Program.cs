47b9df6687328fecdd497ee20d2d18f2dd4e54a4
«qwoizt» <boringfine@gmail.com>
2022-08-21 14:10:55 +0300

    задание 27

---
+++ task27/Program.cs
@@ -0,0 +1,14 @@
+﻿int Summ(int digit)
+{
+   int delDigit = 0;
+   int summDigit = 0;
+   while (digit > 0)
+   {
+        delDigit = digit % 10;
+        summDigit = summDigit + delDigit;
+        digit = digit / 10;
+   } 
+   return summDigit;
+}
+int number = Summ(Convert.ToInt32(Console.ReadLine()));
+Console.WriteLine($"сумма чисел = {number}");
